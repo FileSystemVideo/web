79b5455c877a2f5a9472b0ab0437764fcef2b7f0
File System Video <74895486+FileSystemVideo@users.noreply.github.com>
2021-10-30 15:14:27 +0800

    Add files via upload

---
+++ download.js
@@ -0,0 +1,33 @@
+var appName = 'bql_release_v21.10.22.apk'
+var winName = 'FSV-Setup-21.10.29.exe'
+var linuxName = 'fsv20211030.zip'
+$(function () {
+	$('.js-download').on('click', function () {
+		var url = ''
+		var area = $(this).attr('data-area')
+		var plat = $(this).attr('data-plat')
+		switch (area) {
+			case 'America':
+				url += 'https://dsw3rxqm9wq1x.cloudfront.net/'
+				break
+			case 'Europe':
+				url += 'https://d58qqi0wa6uyo.cloudfront.net/'
+				break
+			default:
+				url += 'http://123.132.234.66:50321/'
+				break
+		}
+		switch (plat) {
+			case 'win':
+				url += area === 'Asia' ? winName : 'pc/' + winName
+				break
+			case 'app':
+				url += area === 'Asia' ? appName : 'app/' + winName
+				break
+			case 'linux':
+				url += 'linux/' + linuxName
+				break
+		}
+    window.location = url
+	})
+})
