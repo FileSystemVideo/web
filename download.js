2d3fb7352ed9dc6344a3f51010676aad295358ac
File System Video <74895486+FileSystemVideo@users.noreply.github.com>
2021-11-07 12:42:23 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,4 +1,4 @@
-var appName = 'bql_release_v21.11.04.sp1.apk'
+var appName = 'bql_release_v21.11.06.sp1.apk'
 var winName = 'FSV-Setup-21.11.5.sp1.exe'
 var linuxName = 'fsv20211030.zip'
 $(function () {
