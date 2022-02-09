3eca840c013c13b38d59a7f7bc8b77e5e31dfc4d
File System Video <74895486+FileSystemVideo@users.noreply.github.com>
2022-02-09 23:37:57 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 var appVersion = '22.01.17'
-var winVersion = '22.2.9'
+var winVersion = '22.1.19'
 var linuxVersion = '22.02.09'
 var appName = 'bql_release_v' + appVersion + '.apk'
 var winName = 'FSV-Setup-' + winVersion + '.exe'
